426a64a6c80376d42ead68e237410202dad41eb2
ShravanJ-123 <163406644+ShravanJ-123@users.noreply.github.com>
2025-01-16 22:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,14 @@ def run_flow(message, account):
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
